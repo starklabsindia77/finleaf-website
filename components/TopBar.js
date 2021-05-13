44452119b58abc67e99e5b1e0e85c7211b8b516d
“Varunps91” <Starklabsindia@gmail.com>
2021-05-13 13:34:03 +0530

    1st commit

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,5 +1,5 @@
 import { PhoneIcon, MailIcon } from "@heroicons/react/outline";
-/* import FacebookIcon from "@material-ui/icons/facebook"; */
+import FacebookIcon from "@material-ui/icons/facebook";
 import InstagramIcon from "@material-ui/icons/instagram";
 import TwitterIcon from "@material-ui/icons/twitter";
 import LinkedInIcon from "@material-ui/icons/linkedin";
@@ -17,7 +17,7 @@ function TopBar() {
         </div>
       </div>
       <div className="flex items-center space-x-4">
-        {/* <FacebookIcon className="h-5 cursor-pointer text-gray-500 border-gray-500 hover:text-blue-500 hover:border-blue-500" /> */}
+        <FacebookIcon className="h-5 cursor-pointer text-gray-500 border-gray-500 hover:text-blue-500 hover:border-blue-500" />
         <TwitterIcon className="h-5 cursor-pointer text-gray-500 border-gray-500 hover:text-blue-500 hover:border-blue-500" />
         <InstagramIcon className="h-5 cursor-pointer text-gray-500 border-gray-500 hover:text-blue-500 hover:border-blue-500" />
         <LinkedInIcon className="h-5 cursor-pointer text-gray-500 border-gray-500 hover:text-blue-500 hover:border-blue-500" />
